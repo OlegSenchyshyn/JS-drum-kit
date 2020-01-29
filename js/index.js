1718f67a002c444ca1c0db442b99223be5fd299b
Oleh Senchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2020-01-29 15:28:50 +0200

    Uploading files of drum-kit project

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function playSound(event) {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`); // .key -> html class="key"
+  if (!audio) return; // stop the function from running all together
+  audio.currentTime = 0; //revind to the start
+  audio.play();
+  key.classList.add('playing'); // JQUERY -> key.addClass('playing')
+}
+function removeTransition(event) {
+  if (event.propertyName !== 'transform') return; // skip if it`s not a transform
+  event.target.classList.remove('playing');
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
