6ee5f3dcfbc9993dd2f66561c46fbfbc4fec3d92
Oleh Senchyshyn <54153346+OlegSenchyshyn@users.noreply.github.com>
2020-03-03 16:42:07 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function playSound(event) {
-  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`); // .key -> html class="key"
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`); //selecting an audio element
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`); //selecting the corresponding key (.key -> html class="key")
   if (!audio) return; // stop the function from running all together
   audio.currentTime = 0; //revind to the start
   audio.play();
@@ -10,7 +10,7 @@ function removeTransition(event) {
   if (event.propertyName !== 'transform') return; // skip if it`s not a transform
   event.target.classList.remove('playing');
 }
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+const keys = document.querySelectorAll('.key'); // or const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition)); //each key gets an EventListener added to it('transitionend'), when transition is ended, we're removing it
 
 window.addEventListener('keydown', playSound);
